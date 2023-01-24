58ca07b5d0ae9cb942635cdf1073caaebcca5714
Terrasse <3305049949@qq.com>
2023-01-24 23:25:20 +0800

    feat!: user system backend almost finish

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 import pymysql
 pymysql.install_as_MySQLdb()
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import wtforms
 
 import os
+import sys
 import json
 from datetime import datetime
+import hashlib
+from typing import Union
+
+db_pass = os.getenv('PASS')
+assert db_pass
 
 app = Flask(__name__)
+app.secret_key = hashlib.md5(db_pass.encode('utf-8')).hexdigest()
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 class Config(object):
     """配置参数"""
     # 设置连接数据库的URL
     user = 'root'
-    password = os.getenv('PASS')
+    password = db_pass
     assert password
     database = 'timeslicing'
     SQLALCHEMY_DATABASE_URI = f'mysql://{user}:{password}@127.0.0.1:3306/{database}'
@@ -35,10 +48,11 @@ class Slice(db.Model):
 	"""时间片"""
 	__tablename__ = 'slices'
 
-	slice_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, comment='时间片编号')
-	time = db.Column(db.DateTime, nullable=False, comment='时间片所在时间起始时间戳')
-	affair = db.Column(db.Unicode(64), nullable=False, comment='时间片对应事务名称')
-	update_time = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), comment='时间片更新时间戳')
+	slice_id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False, comment='时间片编号')
+	user_id = sa.Column(sa.Integer, sa.ForeignKey('users.user_id'), nullable=False, comment='时间片所属用户编号')
+	time = sa.Column(sa.DateTime, nullable=False, comment='时间片所在时间起始时间戳')
+	affair = sa.Column(sa.Unicode(64), nullable=False, comment='时间片对应事务名称')
+	update_time = sa.Column(sa.DateTime, nullable=False, server_default=sa.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), comment='时间片更新时间戳')
 
 	def __repr__(self):
 		return f'<Slice {self.slice_id} {self.time} {self.affair}>'
@@ -50,16 +64,82 @@ class Slice(db.Model):
 			int(self.update_time.timestamp() * 1000)
 		]
 
+class User(db.Model, UserMixin):
+	"""用户"""
+	__tablename__ = 'users'
+
+	user_id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False, comment='用户编号') # note: start value is set in init()
+	username = sa.Column(sa.Unicode(16), nullable=False, comment='用户名')
+	password = sa.Column(sa.Unicode(32), nullable=False, comment='用户密码加盐哈希')
+
+	def __init__(self):
+		super().__init__()
+
+	def __repr__(self):
+		return f'<User {self.user_id} {self.username}>'
+	
+	def verify(self, password: str) -> bool:
+		return self.password == calc_password(self.username, password)
+	
+	def get_id(self) -> int:
+		return self.user_id
+	
+	@staticmethod
+	@login_manager.user_loader
+	def get_by_id(user_id: int) -> Union['User', None]:
+		res: sa.engine.Result = db.session.execute(sa.select(User).where(User.user_id == user_id))
+		return res.scalar() # return None if not found
+	
+	@staticmethod
+	def get_by_name(username: str) -> Union['User', None]:
+		res: sa.engine.Result = db.session.execute(sa.select(User).where(User.username == username))
+		return res.scalar() # return None if not found
+
+
+class LoginForm(wtforms.Form):
+	"""登录表单"""
+	username = wtforms.StringField('用户名', validators=[wtforms.validators.DataRequired()])
+	password = wtforms.PasswordField('密码', validators=[wtforms.validators.DataRequired()])
+
 
 @app.route('/')
 def home():
-	return render_template('home.jinja', title='Home')
+	return render_template('home.jinja', title='Home', user=current_user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = LoginForm(request.form)
+	error_msg = None
+	if request.method == 'POST' and form.validate():
+		username = form.username.data
+		password = form.password.data
+		user = User.get_by_name(username)
+		print(user)
+		if user:
+			if user.verify(password):
+				login_user(user)
+				return redirect(url_for('home'))
+			else:
+				error_msg = '密码错误'
+		else:
+			error_msg = '用户不存在'
+	return render_template('login.jinja', title='登录', form=form, error_msg=error_msg, user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
 
 @app.route('/insert')
+@login_required
 def insert():
-	return render_template('insert.jinja')
+	return render_template('insert.jinja', user=current_user)
 
 @app.post('/insert_post')
+@login_required
 def submit():
 	try:
 		logs_str = request.form['logs']
@@ -78,10 +158,12 @@ def submit():
 		}
 
 @app.route('/inspect')
+@login_required
 def inspect():
-	return render_template('inspect.jinja')
+	return render_template('inspect.jinja', user=current_user)
 
 @app.get('/query')
+@login_required
 def query():
 	try:
 		slices: list[Slice] = Slice.query.all()
@@ -96,11 +178,26 @@ def query():
 			'msg': str(e)
 		}
 
-if __name__ == '__main__':
+def calc_password(username, password):
+	return hashlib.md5((password + username).encode('utf-8')).hexdigest()
+
+def init():
 	with app.app_context():
 		db.drop_all()
 		db.create_all()
+		db.session.execute(sa.text('ALTER TABLE users AUTO_INCREMENT=10000;'))
 		# x=Slice.query.all()
 		# print(x)
 		# print(x[0].time)
 		# print(type(x[0].time))
+
+if __name__ == '__main__':
+	if sys.argv.count("init"):
+		init()
+	if sys.argv.count("register"):
+		with app.app_context():
+			username = input('username: ')
+			password = input('password: ')
+			user = User(username=username, password=calc_password(username, password))
+			db.session.add(user)
+			db.session.commit()
